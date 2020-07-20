18ac7c09691888b867a412cb39a60052d59a92b8
triglyph <triglyph@users.noreply.github.com>
2020-07-20 14:47:42 +0200

    Update helpers.js

--- js/helpers/helpers.js
+++ js/helpers/helpers.js
@@ -1,6 +1,5 @@
 import { uuidv4, uuidv4_string } from './uuidv4.js';
 import { divfactory } from './divfactory.js';
 import { round } from './precisionround.js';
-import { clearElement } from './clearelement.js';
 
-export { uuidv4, uuidv4_string, divfactory, round, clearElement }
+export { uuidv4, uuidv4_string, divfactory, round }
